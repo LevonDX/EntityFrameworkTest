7efe53cb16c7b211aa949d943a06a3dfe0f9e432
Levon Hovhannisyan <lehovhan@microsoft.com>
2023-05-20 20:03:38 +0400

    Add project files.

---
+++ EntityFrameworkTest/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkTest
+{
+    internal class Book // DTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Pages { get; set; }
+    }
+}
---
+++ EntityFrameworkTest/BookDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkTest
+{
+    internal class BookDbContext : DbContext
+    {
+        const string connectionString =
+            "Data Source = localhost; Initial Catalog = BookStoreDbEf; Integrated Security = True; TrustServerCertificate=True";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+
+            this.Database.EnsureCreated();
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
